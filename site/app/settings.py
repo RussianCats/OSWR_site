ed6b4fafbea7426fe9818f8d5e1457565fc22c11
admini <youperdyn@gmail.com>
2024-11-26 21:18:20 +0300

    10.41

--- site/app/settings.py
+++ site/app/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
     'main',
     'goods',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -64,7 +65,7 @@ ROOT_URLCONF = 'app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -149,3 +150,5 @@ INTERNAL_IPS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
---
+++ site/users/views.py
@@ -0,0 +1,46 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.contrib import auth
+from django.urls import reverse
+
+from users.forms import UserLoginForm
+
+def login(request):
+
+    # логиука на вход пользователя
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                # после входа отправляем
+                return HttpResponseRedirect(reverse('main:index'))
+    else:
+        form = UserLoginForm()
+
+    context = {
+        'title': 'Home - Авторизация',
+        'form': form
+    }
+    return render(request, 'users/login.html', context)
+
+def registration(request):
+    context = {
+        'title': 'Home - Регистрация'
+    }
+    return render(request, 'users/registration.html', context)
+
+def profile(request):
+    context = {
+        'title': 'Home - Кабинет'
+    }
+    return render(request, 'users/profile.html', context)
+
+def logout(request):
+    context = {
+        'title': 'Home - Выход'
+    }
+    return render(request, 'users/logout.html', context)
